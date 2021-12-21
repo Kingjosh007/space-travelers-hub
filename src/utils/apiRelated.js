25974f8c1e096a888dc3fc7395d3026d28e5ca15
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-12-21 15:06:25 +0100

    Refactor apiRelated.js

--- src/utils/apiRelated.js
+++ src/utils/apiRelated.js
@@ -1,43 +1,16 @@
-const baseUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi';
+const baseUrl = 'https://api.spacexdata.com/v3/';
 
-const appId = 'rQ2sGk73ALsSsV7Y7oHD';
-const appsEndpoint = `${baseUrl}/apps/`;
-const booksEndpoint = `${appsEndpoint}${appId}/books`;
-const deleteBookEndpoint = (id) => `${booksEndpoint}/${id}`;
-
-const postData = async (url, data, isText = false) => {
-  const res = await fetch(url, {
-    method: 'POST',
-    mode: 'cors',
-    headers: { 'Content-type': 'application/json' },
-    body: JSON.stringify(data),
-  });
-  if (isText) return res.text();
-  return res.json();
-};
-
-const deleteData = async (id) => {
-  const res = await fetch(deleteBookEndpoint(id), {
-    method: 'DELETE',
-    mode: 'cors',
-    headers: { 'Content-type': 'application/json' },
-    body: JSON.stringify({ item_id: id }),
-  });
-  return res.text();
-};
+const rocketsEndpoint = `${baseUrl}/rockets`;
+const missionsEndpoint = `${baseUrl}/missions`;
 
 const getData = async (url) => {
   const res = await fetch(url);
-  const data = await res.json() || {};
-  const dataArr = Object.keys(data).map((key) => ({ item_id: key, ...data[key][0] }));
+  const dataArr = await res.json() || [];
   return dataArr;
 };
 
 export {
-  postData,
+  rocketsEndpoint,
+  missionsEndpoint,
   getData,
-  deleteData,
-  appsEndpoint,
-  booksEndpoint,
-  deleteBookEndpoint,
 };
