ca8b615b875aca4f3117da2a1ecbcce1fbb2e4b0
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-12-21 15:48:53 +0100

    Map fetched rockets to key data

--- src/components/SpaceContainer.js
+++ src/components/SpaceContainer.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { useDispatch, useSelector, Provider } from 'react-redux';
 import Navbar from './Navbar';
 import store from '../redux/configureStore';
 import MyProfile from './pages/MyProfile';
 import RocketPage from './pages/RocketPage';
 import MissionPage from './pages/MissionPage';
 import NotMatch from './pages/NotMatch';
+import { getRockets } from '../redux/rocket/rocket';
 
-const SpaceContainer = () => (
-  <Provider store={store}>
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/Myprofile" element={<MyProfile />} />
-        <Route path="/" element={<RocketPage />} exact />
-        <Route path="/Mission" element={<MissionPage />} />
-        <Route path="*" element={<NotMatch />} />
-      </Routes>
-    </div>
-  </Provider>
-);
+const SpaceContainer = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getRockets());
+  }, [dispatch]);
+
+  const rockets = useSelector((state) => state.rocketReducer);
+  return (
+    <Provider store={store}>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/Myprofile" element={<MyProfile />} />
+          <Route path="/" element={<RocketPage rockets={rockets} />} exact />
+          <Route path="/Mission" element={<MissionPage />} />
+          <Route path="*" element={<NotMatch />} />
+        </Routes>
+      </div>
+    </Provider>
+  );
+};
 
 export default SpaceContainer;
--- src/components/pages/RocketPage.js
+++ src/components/pages/RocketPage.js
@@ -1,4 +1,8 @@
-const RocketPage = () => (
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { getRockets } from '../../redux/rocket/rocket';
+
+const RocketPage = (props) => (
   <div>
     <h3>RocketPage</h3>
   </div>
