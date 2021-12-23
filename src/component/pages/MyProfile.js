aa0708fc2b6ba95bf79cbfe56784e72cb08594d6
mwanawabangona <ellisallday@gmail.com>
2021-12-22 23:30:53 -0800

    Add Js React Redux

--- src/component/pages/MyProfile.js
+++ src/component/pages/MyProfile.js
@@ -1,6 +1,13 @@
+import React from 'react';
+import Profilerocket from './rocketpage/Profilerocket';
+import ProfileDragon from './DragonPage/ProfileDragon';
+import MissionProfile from './missionpage/mission_myprofile';
+
 const MyProfile = () => (
-  <div>
-    <h3>profile</h3>
+  <div className="myProfile">
+    <Profilerocket />
+    <ProfileDragon />
+    <MissionProfile />
   </div>
 );
 export default MyProfile;
